cb8700ef3a8903a07d80068e30809c007e8b2458
Muzammil Ahmed <154408958+MuzammilAhmed27@users.noreply.github.com>
2025-10-27 15:36:15 +0500

    Add files via upload

---
+++ JavaProject/src/newproject/javaproject.java
@@ -0,0 +1,12 @@
+package newproject;
+import java.util.Scanner;
+import java.util.*;
+public class javaproject {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Hello");
+        
+    }
+
+}
