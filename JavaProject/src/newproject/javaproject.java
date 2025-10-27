63ddbee3ddd64e8efad2b256a2c128fb8c355e79
Muzammil Ahmed <154408958+MuzammilAhmed27@users.noreply.github.com>
2025-10-27 15:42:40 +0500

    change in main V2

--- JavaProject/src/newproject/javaproject.java
+++ JavaProject/src/newproject/javaproject.java
@@ -6,7 +6,12 @@ public class javaproject {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Hello");
+		
+		# Changes by Branch V2
+		System.out.println("Roll: 2023F-BSE-045");
+		
         
     }
 
 }
+
